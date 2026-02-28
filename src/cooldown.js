843a7e43f60d7881a4c7bf2d1cd0f5b0f9bdc413
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-02-28 13:18:07 +0400

    Update cooldown.js

--- src/cooldown.js
+++ src/cooldown.js
@@ -1,1 +1,24 @@
+const { COOLDOWN_MINUTES } = require("./limits");
 
+const cooldowns = new Map();
+
+function getRemainingMs(guildId) {
+  const last = cooldowns.get(guildId);
+  if (!last) return 0;
+  const diff = Date.now() - last;
+  const total = COOLDOWN_MINUTES * 60 * 1000;
+  return Math.max(0, total - diff);
+}
+
+function setCooldown(guildId) {
+  cooldowns.set(guildId, Date.now());
+}
+
+function formatRemaining(ms) {
+  const sec = Math.ceil(ms / 1000);
+  const m = Math.floor(sec / 60);
+  const s = sec % 60;
+  return `${m}m ${s}s`;
+}
+
+module.exports = { getRemainingMs, setCooldown, formatRemaining };
