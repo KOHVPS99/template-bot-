2b37ac38be5cf62461d5c0d9941cec36c8af3aaf
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-02-28 13:19:13 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { handleTemplate } = require("./templateHandler");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "template") {
+    await handleTemplate(interaction);
+  }
+});
+
+client.login(process.env.TOKEN);
