e2df6b0c6061b45fa01e02e55173c6a69810dffa
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-02-28 15:02:04 +0400

    Update cooldown.js

--- src/cooldown.js
+++ src/cooldown.js
@@ -2,13 +2,13 @@ const cooldowns = new Map();
 
 module.exports = function(userId) {
   const now = Date.now();
-  const cooldownAmount = 15000;
+  const cooldownTime = 15000;
 
   if (cooldowns.has(userId)) {
     const expiration = cooldowns.get(userId);
     if (now < expiration) return false;
   }
 
-  cooldowns.set(userId, now + cooldownAmount);
+  cooldowns.set(userId, now + cooldownTime);
   return true;
 };
