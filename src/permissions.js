6a711513f94a38affbdc41d739b5ba57e235b038
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-02-28 13:18:21 +0400

    Update permissions.js

--- src/permissions.js
+++ src/permissions.js
@@ -1,1 +1,18 @@
+const { PermissionsBitField } = require("discord.js");
 
+function checkUser(interaction) {
+  if (!interaction.memberPermissions.has(PermissionsBitField.Flags.ManageGuild)) {
+    return "You need Manage Server permission.";
+  }
+  return null;
+}
+
+function checkBot(guild) {
+  const me = guild.members.me;
+  if (!me.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+    return "I need Manage Channels permission.";
+  }
+  return null;
+}
+
+module.exports = { checkUser, checkBot };
