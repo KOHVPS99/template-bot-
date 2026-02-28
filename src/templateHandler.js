2e33b846fcd8c7356d3cfa988e69ba18f7fb136e
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-02-28 15:01:50 +0400

    Update templateHandler.js

--- src/templateHandler.js
+++ src/templateHandler.js
@@ -1,14 +1,19 @@
-const { ChannelType } = require("discord.js");
+const { ChannelType, PermissionsBitField } = require("discord.js");
 
-module.exports = async function(guild, template) {
+module.exports = async function(guild, template, includeStaff) {
+
+  // CREATE ROLES
+  const createdRoles = {};
 
   for (const role of template.roles) {
-    await guild.roles.create({
+    const newRole = await guild.roles.create({
       name: role.name,
       color: role.color
     });
+    createdRoles[role.name] = newRole;
   }
 
+  // CREATE CATEGORIES + CHANNELS
   for (const cat of template.categories) {
 
     const category = await guild.channels.create({
@@ -23,5 +28,32 @@ module.exports = async function(guild, template) {
         parent: category.id
       });
     }
+  }
+
+  // OPTIONAL STAFF CATEGORY
+  if (includeStaff) {
+
+    const staffRole = createdRoles["👑 Owner"] || Object.values(createdRoles)[0];
+
+    const staffCategory = await guild.channels.create({
+      name: "🔒 staff",
+      type: ChannelType.GuildCategory,
+      permissionOverwrites: [
+        {
+          id: guild.roles.everyone.id,
+          deny: [PermissionsBitField.Flags.ViewChannel]
+        },
+        {
+          id: staffRole.id,
+          allow: [PermissionsBitField.Flags.ViewChannel]
+        }
+      ]
+    });
+
+    await guild.channels.create({
+      name: "staff-chat",
+      type: ChannelType.GuildText,
+      parent: staffCategory.id
+    });
   }
 };
